c1fc3ac3b42e8551610663816d10cd9ce9e4255a
sarahlia <sarahlia23@outlook.com>
2021-09-14 22:58:19 -0500

    initial refactoring.

---
+++ code/adventure_game.py
@@ -0,0 +1,14 @@
+import random
+
+progression = True
+
+while progression:
+    # print("Welcome to Alexandria!")
+    ans = input("Do you want to start the game? [y/n]")
+
+    if ans.lower() == "y":
+        print("testing")
+        break
+    else:
+        print("Have a great day!")
+        progression = False
