f67fe22327b850a139ca80070b21084854f11c90
sarahlia <sarahlia23@outlook.com>
2021-10-22 14:50:01 -0500

    modified networks.tf

--- code/aws_deployment/networks.tf
+++ code/aws_deployment/networks.tf
@@ -1,3 +1,68 @@
+#Create VPC in us-east-1
+resource "aws_vpc" "vpc_master" {
+  provider             = aws.region-master
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "master-vpc-jenkins"
+  }
+
+}
+
+#Create VPC in us-west-2
+resource "aws_vpc" "vpc_master_oregon" {
+  provider             = aws.region-worker
+  cidr_block           = "192.168.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "worker-vpc-jenkins"
+  }
+
+}
+
+#Create IGW in us-east-1
+resource "aws_internet_gateway" "igw" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+}
+
+#Create IGW in us-west-2
+resource "aws_internet_gateway" "igw-oregon" {
+  provider = aws.region-worker
+  vpc_id   = aws_vpc.vpc_master_oregon.id
+}
+
+#Get all available AZ's in VPC for master region
+data "aws_availability_zones" "azs" {
+  provider = aws.region-master
+  state    = "available"
+}
+
+#Create subnet # 1 in us-east-1
+resource "aws_subnet" "subnet_1" {
+  provider          = aws.region-master
+  availability_zone = element(data.aws_availability_zones.azs.names, 0)
+  vpc_id            = aws_vpc.vpc_master.id
+  cidr_block        = "10.0.1.0/24"
+}
+
+#Create subnet #2  in us-east-1
+resource "aws_subnet" "subnet_2" {
+  provider          = aws.region-master
+  vpc_id            = aws_vpc.vpc_master.id
+  availability_zone = element(data.aws_availability_zones.azs.names, 1)
+  cidr_block        = "10.0.2.0/24"
+}
+
+#Create subnet in us-west-2
+resource "aws_subnet" "subnet_1_oregon" {
+  provider   = aws.region-worker
+  vpc_id     = aws_vpc.vpc_master_oregon.id
+  cidr_block = "192.168.1.0/24"
+}
+
 #Initiate Peering connection request from us-east-1
 resource "aws_vpc_peering_connection" "useast1-uswest2" {
   provider    = aws.region-master
@@ -48,7 +113,7 @@ resource "aws_route_table" "internet_route_oregon" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw-oregon.id
-  }
+  } 
   route {
     cidr_block                = "10.0.1.0/24"
     vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
